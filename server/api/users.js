94299249217cf70003f7b6dd4c7b1f1a41823670
tiffanyannwiley <113059262+tiffanyannwiley@users.noreply.github.com>
2023-03-02 20:31:21 -0600

    added userinfo apis, update user account api, user account api

--- server/api/users.js
+++ server/api/users.js
@@ -14,4 +14,49 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
     next(err)
   }
+});
+
+router.get('/:id/account', async (req, res, next) => {
+  try {
+    res.send(await User.findByPk(req.params.id));
+  } catch (err) {
+    console.log('Can not find user', err);
+    next(err)
+  }
+});
+
+router.put('/:id/account', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.send(await user.update(req.body));
+  } catch(err) {
+    console.log('Can not update user', err);
+    next(err)
+  }
+});
+
+router.post('/:id/userinfo', async (req, res, next) => {
+  try {
+    const info = await UserInfo.create(req.body);
+    info.userId = req.params.id;
+    await info.save();
+    res.status(201).send(info);
+  } catch (err) {
+    console.log('Can not add user information');
+    next(err)
+  }
+});
+
+router.put('/id/userinfo', async (req, res, next) => {
+  try {
+    const info = await UserInfo.findAll({
+      where: {
+        userId: req.params.id
+      }
+    });
+    res.send(await info.update(req.body));
+  } catch(err) {
+    console.log('Can not update user information', err);
+    next(err)
+  }
 })
