094e57c234c90e908b8e318938f55855a73a6a11
HumbertoNelson <48134908+HumbertoNelson@users.noreply.github.com>
2023-10-01 19:33:56 -0400

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 //this is the access point for all things database related!
 
 const db = require("./db");
+require("dotenv").config();
 
 const User = require("./models/User");
 const UserInfo = require("./models/UserInfo");
